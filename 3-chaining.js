bc0b96bec16a8c25d340392bc61ac0ca27fff573
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-05-31 22:30:00 -0700

    Mode chains & zombies

--- 3-chaining.js
+++ 3-chaining.js
@@ -3,61 +3,70 @@ const { myPromise } = require("./myPromise");
 // What happens if we return stuff from the "then"
 myPromise(1)
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
     return "Hello World";
   })
   .then(result => console.log(result))  // --> The "stuff" ends up as a result in the next "then"
-  .catch(e => console.log(e));
+  .catch(e => console.log("catch: ", e));
 
 // If we return a promise it executes afer the 1st one. No more waterfalls!!
 myPromise(1)
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
     return myPromise(1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
   })
-  .catch(e => console.log(e));
+  .catch(e => console.log("catch: ", e));
 
 // We can chain lots of promises. No more waterfalls!!
-// The trick is in the return from the "then"
+// The trick is to return a new promise from the "then"
 myPromise(1)
   .then(result => {
-    console.log(result);
+    console.log("then 1:", result);
     return myPromise(1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then 2:", result);
     return myPromise(1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then 3:", result);
     return myPromise(1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then 4:", result);
   })
-  .catch(e => console.log(e));
+  .catch(e => console.log("catch: ", e));
 
 // But if you return a promise, be sure to catch any errors later in the chain. If you dont ...
 myPromise(1)
-  .catch(e => console.log(e))
+  .catch(e => console.log("catch: ", e))
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
     return myPromise(-1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
   });
 
 // Usually we put catch at the end. Otherwise subsequent then's may grab its return if it fires
 myPromise(1)
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
     return myPromise(-1);
   })
   .then(result => {
-    console.log(result);
+    console.log("then:", result);
   })
-  .catch(e => console.log(e));  // Lets see what happens if we move this before 2nd then 
+  .catch(e => console.log("catch:", e));  // Lets see what happens if we move this before 2nd then 
+
+// How about here here?
+myPromise(1)
+  .then(result => result)
+  .then(result => console.log("then:", result))
+  .then(result => console.log("then:", result)) // --> Zombie "then" was fired. Why??
+  .catch(e => console.log("catch:", e));
+
+  // Its all about what happens with the return from a "then" or a "catch"
--- 4-moreChains.js
+++ 4-moreChains.js
@@ -1,6 +1,6 @@
 const { myPromise } = require("./myPromise");
 
-// Lets look at the promise chain a little more closely.  What happens here?
+// // Lets look at the "then" chain a little more closely.  What happens here?
 myPromise(1)
   .then(result => result)
   .then(result => result)
@@ -8,11 +8,14 @@ myPromise(1)
   .then(result => result)
   .then(result => 5)
   .then(result => result)
-  .then(result => console.log(result))
-  .catch(e => console.log(e));
+  .then(result => console.log("then:", result))
+  .catch(e => console.log("catch:", e));
+
+
+
 
 // How about this?  Just return the error from the catch
 myPromise(-1)
   .catch(e => e)
-  .then(result => console.log("Result:", result)); // hey! The "then" got called!  Why?
+  .then(result => console.log("then:", result)); // hey! The "then" fired!  Why?
 
