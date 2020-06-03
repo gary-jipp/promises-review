b42eb7b0fb814f819609dbc406b4f9e919846c85
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-06-02 18:43:20 -0700

    format

--- 7-profoundPromises.js
+++ 7-profoundPromises.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 
 const url = 'https://api.kanye.rest/';
+
+
+// Be careful doing this. Don't want to create too many
 const promises = [];
 for (let i = 0; i < 10; i++) {
   promises[i] = axios.get(url);
